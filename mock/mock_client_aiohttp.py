06fff93afe809ed53ca0db2a4dd67daad78b5bbb
calvinkuo <14984741+calvinkuo@users.noreply.github.com>
2022-12-14 20:23:17 -0500

    Update tests

--- mock/mock_client_aiohttp.py
+++ mock/mock_client_aiohttp.py
@@ -1,14 +1,18 @@
 import asyncio
+import timeit
 
 import aiohttp
 
 
 async def main():
-    async with aiohttp.ClientSession() as session:
-        async with session.get('http://example.com/', proxy='http://127.0.0.1:9999') as r:
-            print(await r.read())
-        async with session.get('https://example.com/', proxy='http://127.0.0.1:9999') as r2:
-            print(await r2.read())
+    for _ in range(3):
+        async with aiohttp.ClientSession() as session:
+            start = timeit.default_timer()
+            async with session.get('https://example.com/', proxy='http://127.0.0.1:60123') as r:
+                data = await r.read()
+            end = timeit.default_timer()
+            # print(data)
+            print(f'Took {end - start} seconds for {len(data)} bytes')
 
 
 if __name__ == '__main__':
--- mock/mock_client_slow.py
+++ mock/mock_client_slow.py
@@ -1,27 +1,36 @@
 import asyncio
 
-payload = b'GET http://example.com/ HTTP/1.1\r\nHost: example.com\r\nAccept: */*\r\n' \
-          b'User-Agent: Python/3.10 aiohttp/3.8.3\r\n\r\n'
+payload = b'GET http://example.com/ HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n'
+# payload = b'GET http://example.com/?test=' + (b'test' * (1024*1024)) + b' HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\n\r\n'  # long URL
+# payload = b'GET http://example.com/ HTTP/1.1\r\nHost: example.com\r\nConnection: close\r\nX-Test: ' + (b'test' * (1024*1024)) + b'\r\n\r\n'  # long header
 
 
 async def main():
-    s = [await asyncio.open_connection('127.0.0.1', 9999) for _ in range(1)]
-    for i in payload:
-        print(repr(bytes([i]))[2:-1], end='')
-        for reader, writer in s:
-            writer.write(bytes([i]))
-            await writer.drain()
-        await asyncio.sleep(0.1)
+    s = [await asyncio.open_connection('127.0.0.1', 60123) for _ in range(200)]
+    try:
+        for i in payload:
+            print(repr(bytes([i]))[2:-1], end='')
+            for reader, writer in s:
+                writer.write(bytes([i]))
+                await writer.drain()
+                if writer.is_closing():
+                    break
+            await asyncio.sleep(0.1)
+
+        # for reader, writer in s:
+        #     writer.write(payload)
+        # for reader, writer in s:
+        #     await writer.drain()
+        #     if writer.is_closing():
+        #         break
+    except ConnectionError:
+        pass
     print()
     for reader, writer in s:
-        data = await reader.read(8192)
+        data = await reader.read()
         print(data)
-    # with open('test.html', 'wb') as f:
-    #     f.write(data)
-    # print(await reader2.read(8192))
-    await asyncio.sleep(100)
-    # for reader, writer in s:
-    #     writer.close()
+    for reader, writer in s:
+        writer.close()
 
 
 if __name__ == '__main__':
